bd3f2e9876c48c76a6857efeef412f531e975203
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-28 19:37:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,21 +88,3 @@ def all_holidays_with_bbq(holiday_hash)
   }
   holidas_with_bbq
 end
-
-
-holiday_hash = {
-   :winter => {
-     :christmas => ["Lights", "Wreath"],
-     :new_years => ["Party Hats"]
-   },
-   :summer => {
-     :fourth_of_july => ["Fireworks", "BBQ"]
-   },
-   :fall => {
-     :thanksgiving => ["Turkey"]
-   },
-   :spring => {
-     :memorial_day => ["BBQ"]
-   }
- }
-all_supplies_in_holidays(holiday_hash)
