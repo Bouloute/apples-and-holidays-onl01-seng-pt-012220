0295e201182f0a75329d5e87cf6abdb80a2909cd
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-28 17:53:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   all_winter_supplies = []
   holiday_hash[:winter].each{|holiday, supply| all_winter_supplies.push(supply)}
-  all_winter_supplies
+  puts all_winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
