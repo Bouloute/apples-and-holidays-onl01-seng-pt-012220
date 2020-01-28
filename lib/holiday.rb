974a5a124e5c38a810b9696af4918ac2d82f36cf
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-28 18:13:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holidays.each{|holiday_name, supplies|
 
-        puts "  #{holiday_name.capitalize}: #{supplies}"
+        puts "  #{holiday_name.capitalize}: #{supplies.flatten}"
 
     }
   }
